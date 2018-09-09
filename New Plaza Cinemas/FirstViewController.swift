eaaf714791714ff0c6022722008f34574563fd3b
Zvonko Nikolich <lureofthesea@gmail.com>
2018-09-08 23:05:26 -0400

    Initial Commit

---
+++ New Plaza Cinemas/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  New Plaza Cinemas
+//
+//  Created by Zvonko on 9/8/18.
+//  Copyright © 2018 ZvoNik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ New Plaza Cinemas/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  New Plaza Cinemas
+//
+//  Created by Zvonko on 9/8/18.
+//  Copyright © 2018 ZvoNik. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
